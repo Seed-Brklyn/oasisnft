b5edcdf95033c7e0ec3cdbd58b3343af8f31fd0d
jawauntb <jawaun.brown95@gmail.com>
2022-08-02 13:06:00 -0400

    starting config for foundry build

---
+++ src/test/SeedNFT.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import "ds-test/test.sol";
+import {console} from "forge-std/console.sol";
+import {stdStorage, StdStorage, Test} from "forge-std/Test.sol";
+
+contract SeedTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
